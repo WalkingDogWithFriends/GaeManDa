2a68854a6f6359f47942f692598c6941c478abff
Jung SeokYoung <thrdud0423@gmail.com>
2023-04-27 20:03:04 +0900

    Initial Commits

---
+++ GaeManda/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GaeManda
+//
+//  Created by jung on 2023/04/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
