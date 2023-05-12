b5deb43cf4ed485af35a2960e5d9d03a2826f776
jungseokyoung-cloud <thrdud0423@gmail.com>
2023-05-12 12:14:11 +0900

    SignIn Dependency 수정 및 Target함수 수정

--- GaeManda/Projects/Presentation/SignIn/Project.swift
+++ GaeManda/Projects/Presentation/SignIn/Project.swift
@@ -6,26 +6,24 @@ private let projectName = "SignIn"
 let project = Project.createProject(
 	name: projectName,
 	targets: [
-		.createTarget(
-			name: "SignIn",
-			product: .framework,
-			sources: ["Interfaces/**"],
-			dependencies: [
-				.Project.CoreLayer.Extensions,
-				.Project.DomainLayer.Entity,
-				.Project.DomainLayer.UseCase
-			]
-		),
-		.createTarget(
-			name: "SignInImpl",
-			product: .staticLibrary,
-			sources: ["Implementations/**"],
-			dependencies: [
-				.Project.PresentationLayer.SignIn,
-				.Project.DesignKit,
-				.SPM.RIBs,
-			]
-		)
+        .createIntefaceTarget(
+            name: projectName,
+            dependencies: [
+                .Project.DomainLayer.Entity,
+                .SPM.RIBs
+            ]
+        ),
+        
+        .createImplementationTarget(
+            name: projectName,
+            dependencies: [
+                .Project.PresentationLayer.SignIn,
+                .Project.DesignKit,
+                .Project.CoreLayer.Extensions,
+                .Project.DomainLayer.UseCase
+            ]
+        ),
+        .createTestTarget(name: projectName)
 	]
 )
 
