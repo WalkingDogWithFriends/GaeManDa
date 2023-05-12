6dff3d837c7394875538c4120b9394f8e4079d7c
jungseokyoung-cloud <thrdud0423@gmail.com>
2023-05-12 12:12:27 +0900

    Settings Dependeny 수정 및 Target함수 수정

--- GaeManda/Projects/Presentation/Settings/Project.swift
+++ GaeManda/Projects/Presentation/Settings/Project.swift
@@ -6,25 +6,23 @@ private let projectName = "Settings"
 let project = Project.createProject(
 	name: projectName,
 	targets: [
-		.createTarget(
-			name: "Settings",
-			product: .framework,
-			sources: ["Interfaces/**"],
-			dependencies: [
-				.Project.CoreLayer.Extensions,
-				.Project.DomainLayer.Entity,
-				.Project.DomainLayer.UseCase
-			]
-		),
-		.createTarget(
-			name: "SettingsImpl",
-			product: .staticLibrary,
-			sources: ["Implementations/**"],
-			dependencies: [
-				.Project.PresentationLayer.Settings,
-				.Project.DesignKit,
-				.SPM.RIBs,
-			]
-		)
+        .createIntefaceTarget(
+            name: projectName,
+            dependencies: [
+                .Project.DomainLayer.Entity,
+                .SPM.RIBs
+            ]
+        ),
+        
+        .createImplementationTarget(
+            name: projectName,
+            dependencies: [
+                .Project.PresentationLayer.Settings,
+                .Project.DesignKit,
+                .Project.CoreLayer.Extensions,
+                .Project.DomainLayer.UseCase
+            ]
+        ),
+        .createTestTarget(name: projectName)
 	]
 )
