edb2b33a0b6048126ae0c2e3d9dffd713d7e77d3
김영균 <kyoungyun98@naver.com>
2023-05-14 02:29:21 +0900

    [STYLE] : 충돌 해결

--- GaeManda/Projects/App/Project.swift
+++ GaeManda/Projects/App/Project.swift
@@ -2,54 +2,56 @@ import ProjectDescription
 import ProjectDescriptionHelpers
 
 let project = Project.createProject(
-  name: "GaeManda",
-  targets: [
-    .createTarget(
-      name: "Dev-GaeManda",
-      product: .app,
-      infoPlist: .file(path: .relativeToRoot("Projects/App/Info.plist")),
-      resources: ["Resources/**"],
-      dependencies: [
-        .Project.PresentationLayer.SettingsImpl,
-        .Project.PresentationLayer.ChattingImpl,
-        .Project.PresentationLayer.SignInImpl,
-        .Project.PresentationLayer.SignUpImpl,
-        .Project.PresentationLayer.DogsOnAroundImpl,
-        .SPM.RIBs
-      ],
-      settings: .settings(
-        base: [
-          "ASSETCATALOG_COMPILER_APPICON_NAME":"DevAppIcon"
-        ],
-        configurations: [
-          .debug(name: .debug, xcconfig: "./xcconfigs/GaeManda.debug.xcconfig")
-        ]
-      )
-    ),
-    .createTarget(
-      name: "Prod-GaeManda",
-      product: .app,
-      infoPlist: .file(path: .relativeToRoot("Projects/App/Info.plist")),
-      resources: ["Resources/**"],
-      dependencies: [
-        .Project.PresentationLayer.SettingsImpl,
-        .Project.PresentationLayer.ChattingImpl,
-        .Project.PresentationLayer.SignInImpl,
-        .Project.PresentationLayer.SignUpImpl,
-        .Project.PresentationLayer.DogsOnAroundImpl,
-        .SPM.RIBs
-      ],
-      settings: .settings(
-        base: [
-          "ASSETCATALOG_COMPILER_APPICON_NAME":"ProdAppIcon"
-        ],
-        configurations: [
-          .release(name: .release, xcconfig: "./xcconfigs/GaeManda.release.xcconfig")
-        ]
-      )
-    )
-  ],
-  additionalFiles: [
-    "./xcconfigs/GaeManda.shared.xcconfig"
-  ]
+	name: "GaeManda",
+	targets: [
+		.createTarget(
+			name: "Dev-GaeManda",
+			product: .app,
+			bundleId: "com.gaemanda.dev",
+			infoPlist: .file(path: .relativeToRoot("Projects/App/Info.plist")),
+			resources: ["Resources/**"],
+			dependencies: [
+				.Project.PresentationLayer.SettingsImpl,
+				.Project.PresentationLayer.ChattingImpl,
+				.Project.PresentationLayer.SignInImpl,
+				.Project.PresentationLayer.SignUpImpl,
+				.Project.PresentationLayer.DogsOnAroundImpl,
+				.SPM.RIBs
+			],
+			settings: .settings(
+				base: [
+					"ASSETCATALOG_COMPILER_APPICON_NAME":"DevAppIcon"
+				],
+				configurations: [
+					.debug(name: .debug, xcconfig: "./xcconfigs/GaeManda.debug.xcconfig")
+				]
+			)
+		),
+		.createTarget(
+			name: "Prod-GaeManda",
+			product: .app,
+			bundleId: "com.gaemanda",
+			infoPlist: .file(path: .relativeToRoot("Projects/App/Info.plist")),
+			resources: ["Resources/**"],
+			dependencies: [
+				.Project.PresentationLayer.SettingsImpl,
+				.Project.PresentationLayer.ChattingImpl,
+				.Project.PresentationLayer.SignInImpl,
+				.Project.PresentationLayer.SignUpImpl,
+				.Project.PresentationLayer.DogsOnAroundImpl,
+				.SPM.RIBs
+			],
+			settings: .settings(
+				base: [
+					"ASSETCATALOG_COMPILER_APPICON_NAME":"ProdAppIcon"
+				],
+				configurations: [
+					.release(name: .release, xcconfig: "./xcconfigs/GaeManda.release.xcconfig")
+				]
+			)
+		)
+	],
+	additionalFiles: [
+		"./xcconfigs/GaeManda.shared.xcconfig"
+	]
 )
--- GaeManda/Tuist/ProjectDescriptionHelpers/Target+Extension.swift
+++ GaeManda/Tuist/ProjectDescriptionHelpers/Target+Extension.swift
@@ -1,79 +1,82 @@
 import ProjectDescription
 
 public extension Target {
-  static func createImplementationTarget(
-    name: String,
-    dependencies: [TargetDependency] = []
-  ) -> Target {
-    return .init(
-      name: "\(name)Impl",
-      platform: .iOS,
-      product: .staticLibrary,
-      bundleId: "com.gmd.app.\(name)Impl",
-      deploymentTarget: .iOS(targetVersion: "15.0", devices: [.iphone]),
-      infoPlist: .default,
-      sources: ["Implementations/**"],
-      dependencies: dependencies
-    )
-  }
-  
-  static func createIntefaceTarget(
-    name: String,
-    dependencies: [TargetDependency] = []
-  ) -> Target {
-    return .init(
-      name: name,
-      platform: .iOS,
-      product: .framework,
-      bundleId: "com.gmd.app.\(name)",
-      deploymentTarget: .iOS(targetVersion: "15.0", devices: [.iphone]),
-      infoPlist: .default,
-      sources: ["Interfaces/**"],
-      dependencies: dependencies
-    )
-  }
-  
-  static func createTestTarget(
-    name: String
-  ) -> Target {
-    return .init(
-      name: "\(name)Tests",
-      platform: .iOS,
-      product: .unitTests,
-      bundleId: "com.gmd.app.\(name)Tests",
-      deploymentTarget: .iOS(targetVersion: "15.0", devices: [.iphone]),
-      infoPlist: .default,
-      sources: ["Tests/**"],
-      dependencies: [
-        .target(name: name + "Impl")
-      ]
-    )
-  }
+	static func createImplementationTarget(
+		name: String,
+		dependencies: [TargetDependency] = []
+	) -> Target {
+		return .init(
+			name: "\(name)Impl",
+			platform: .iOS,
+			product: .staticLibrary,
+			bundleId: "com.gaemanda.\(name)Impl",
+			deploymentTarget: .iOS(targetVersion: "15.0", devices: [.iphone]),
+			infoPlist: nil,
+			sources: ["Implementations/**"],
+			dependencies: dependencies
+		)
+	}
+	static func createIntefaceTarget(
+		name: String,
+		dependencies: [TargetDependency] = []
+	) -> Target {
+		return .init(
+			name: name,
+			platform: .iOS,
+			product: .framework,
+			bundleId: "com.gaemanda.\(name)",
+			deploymentTarget: .iOS(targetVersion: "15.0", devices: [.iphone]),
+			infoPlist: nil,
+			sources: ["Interfaces/**"],
+			dependencies: dependencies
+		)
+	}
+	static func createTestTarget(
+		name: String
+	) -> Target {
+		return .init(
+			name: "\(name)Tests",
+			platform: .iOS,
+			product: .unitTests,
+			bundleId: "com.gaemanda.\(name)Tests",
+			deploymentTarget: .iOS(targetVersion: "15.0", devices: [.iphone]),
+			infoPlist: nil,
+			sources: ["Tests/**"],
+			dependencies: [
+				.target(name: name + "Imp")
+			]
+		)
+	}
 }
 
 public extension Target{
-  static func createTarget(
-    name: String,
-    platform: Platform = .iOS,
-    product: Product,
-    deploymentTarget: DeploymentTarget = .iOS(targetVersion: "15.0", devices: [.iphone]),
-    infoPlist: InfoPlist = .default,
-    sources: SourceFilesList = ["Sources/**"],
-    resources: ResourceFileElements? = nil,
-    dependencies: [TargetDependency] = [],
-    settings: Settings? = nil
-  ) -> Target {
-    return .init(
-      name: name,
-      platform: platform,
-      product: product,
-      bundleId: "com.gmd.app.\(name)",
-      deploymentTarget: deploymentTarget,
-      infoPlist: infoPlist,
-      sources: sources,
-      resources: resources,
-      dependencies: dependencies,
-      settings: settings
-    )
-  }
+	static func createTarget(
+		name: String,
+		platform: Platform = .iOS,
+		product: Product,
+		bundleId: String? = nil,
+		deploymentTarget: DeploymentTarget = .iOS(targetVersion: "15.0", devices: [.iphone]),
+		infoPlist: InfoPlist = .default,
+		sources: SourceFilesList = ["Sources/**"],
+		resources: ResourceFileElements? = nil,
+		dependencies: [TargetDependency] = [],
+		settings: Settings? = nil
+	) -> Target {
+		var defaultBundleId = "com.gaemanda.\(name)"
+		if let bingingBundleId = bundleId {
+			defaultBundleId = bingingBundleId
+		}
+		return .init(
+			name: name,
+			platform: platform,
+			product: product,
+			bundleId: defaultBundleId,
+			deploymentTarget: deploymentTarget,
+			infoPlist: infoPlist,
+			sources: sources,
+			resources: resources,
+			dependencies: dependencies,
+			settings: settings
+		)
+	}
 }
