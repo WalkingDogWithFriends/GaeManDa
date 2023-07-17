4e03db5417f737098f8fa01ea69500957d74ff0b
jungseokyoung-cloud <thrdud0423@gmail.com>
2023-07-17 18:18:38 +0900

    [INFRA]: GMDProfile 모듈 추가

---
+++ GaeManda/Projects/Presentation/GMDProfile/Project.swift
@@ -0,0 +1,29 @@
+import ProjectDescription
+import ProjectDescriptionHelpers
+
+private let projectName = "GMDProfile"
+
+let project = Project.createProject(
+	name: projectName,
+	targets: [
+		.createIntefaceTarget(
+			name: projectName,
+			dependencies: [
+				.Project.DomainLayer.Entity,
+				.Project.CoreLayer.GMDUtils,
+				.SPM.RIBs
+			]
+		),
+		.createImplementationTarget(
+			name: projectName,
+			dependencies: [
+				.Project.PresentationLayer.GMDProfile,
+				.Project.DesignKit,
+				.Project.CoreLayer.GMDExtensions,
+				.Project.DomainLayer.UseCase,
+				.SPM.RxCocoa
+			]
+		),
+		.createTestTarget(name: projectName)
+	]
+)
