87239b2d8bbcd489212e78f673d315824485a896
jungseokyoung-cloud <thrdud0423@gmail.com>
2023-08-02 00:00:39 +0900

    [FEAT]: DogProfileDashBoard 추가

---
+++ GaeManda/Projects/Presentation/GMDProfile/Implementations/DogProfileEdit/Widgets/DogProfileDashBoardCell.swift
@@ -0,0 +1,47 @@
+//
+//  DogProfileDashBoardCell.swift
+//  GMDProfileImpl
+//
+//  Created by jung on 2023/08/01.
+//  Copyright © 2023 com.gaemanda. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+import DesignKit
+
+final class DogProfileDashBoardCell: UICollectionViewCell {
+	static let identifier = "DogProfileDashBoardCell"
+	
+	lazy var isEdited: Bool = false {
+		didSet {
+			imageView.layer.borderWidth = isEdited ? 1 : 0
+		}
+	}
+	
+	let imageView = RoundImageView()
+	
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+	}
+		
+	required init?(coder: NSCoder) {
+		super.init(coder: coder)
+	}
+	
+	override func prepareForReuse() {
+		super.prepareForReuse()
+	}
+
+	func configuration(image: UIImage?) {
+		imageView.image = image
+		
+		contentView.addSubview(imageView)
+		imageView.layer.borderColor = UIColor.green100.cgColor
+		
+		imageView.snp.makeConstraints { make in
+			make.height.width.equalTo(56)
+			make.top.bottom.leading.trailing.equalToSuperview()
+		}
+	}
+}
