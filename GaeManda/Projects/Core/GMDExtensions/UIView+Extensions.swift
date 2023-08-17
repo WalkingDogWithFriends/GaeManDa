11f5b67ae25bc15d58b4b5aae03c81be935b2a58
younggyun <kyoungyun98@naver.com>
2023-08-17 18:38:01 +0900

    [FEAT] : Add static identifier

--- GaeManda/Projects/Core/GMDExtensions/UIView+Extensions.swift
+++ GaeManda/Projects/Core/GMDExtensions/UIView+Extensions.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 public extension UIView {
+	/// The identifier for the view. (ex: `MyView.identifier`)	
+	static var identifier: String {
+		String(describing: self)
+	}
+	
 	/// Adds multiple subviews to the view.
 	/// - Parameter views: The views to add as subviews. (ex: `view.addSubviews(view1, view2, view3)`)
 	func addSubviews(_ views: UIView...) {
