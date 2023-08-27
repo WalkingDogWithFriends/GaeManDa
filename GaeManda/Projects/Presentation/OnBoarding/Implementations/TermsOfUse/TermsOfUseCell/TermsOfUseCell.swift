c501ddc2188d72a4c91ccb0ee41b0fe5af3aa47a
younggyun <kyoungyun98@naver.com>
2023-08-28 00:18:16 +0900

    [REFACTOR]: TermOfUseView 리팩토링

--- GaeManda/Projects/Presentation/OnBoarding/Implementations/TermsOfUse/TermsOfUseCell/TermsOfUseCell.swift
+++ GaeManda/Projects/Presentation/OnBoarding/Implementations/TermsOfUse/TermsOfUseCell/TermsOfUseCell.swift
@@ -7,75 +7,69 @@
 //
 
 import UIKit
-import RxCocoa
-import RxSwift
 import SnapKit
 import DesignKit
 import Entity
 import GMDUtils
 
 final class TermsOfUseCell: UITableViewCell {
-	var disposeBag = DisposeBag()
+	// MARK: - UI Components
+	private let termsOfUseButton = TermsOfUseButton()
+	private let subTitleLabel = UILabel()
 	
-	lazy var checkBoxButtonTap: Observable<Void> = {
-		return termsOfUseButton.checkButton.rx.tap.asObservable()
-	}()
-	
-	let termsOfUseButton: TermsOfUseButton = {
-		let button = TermsOfUseButton()
-		
-		return button
-	}()
-	
-	lazy var subTitleLabel: UILabel = {
-		let label = UILabel()
-		label.textColor = .gray80
-		label.font = .r12
-		
-		return label
-	}()
-	
+	// MARK: - Initializers
 	override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
 		super.init(style: style, reuseIdentifier: reuseIdentifier)
 		self.selectionStyle = .none
+		self.termsOfUseButton.isUserInteractionEnabled = false
+		self.setupUI()
 	}
 	
+	@available(*, unavailable)
 	required init?(coder: NSCoder) {
 		fatalError()
 	}
-	
-	private func setupUI(termsOfUse: TermsOfUse) {
-		let prefix = termsOfUse.isRequired ? "[필수]" : "[선택]"
-		
-		termsOfUseButton.setTitle("\(prefix) \(termsOfUse.title)")
-		subTitleLabel.text = termsOfUse.subTitle
-		
-		setupSubviews()
+}
+
+// MARK: - UI Methods
+private extension TermsOfUseCell {
+	func setupUI() {
+		setViewHierarchy()
 		setConstraints()
 	}
 	
-	private func setupSubviews() {
-		contentView.addSubview(termsOfUseButton)
-		contentView.addSubview(subTitleLabel)
+	func setViewHierarchy() {
+		contentView.addSubviews(termsOfUseButton, subTitleLabel)
 	}
 	
-	private func setConstraints() {
+	func setConstraints() {
 		termsOfUseButton.snp.makeConstraints { make in
 			make.top.leading.trailing.equalToSuperview()
+			make.height.equalTo(44)
 		}
 		
 		subTitleLabel.snp.makeConstraints { make in
 			make.top.equalTo(termsOfUseButton.snp.bottom)
 			make.bottom.equalToSuperview()
-			make.leading.equalTo(termsOfUseButton.snp.leading)
+			make.leading.equalToSuperview().offset(52)
+			make.trailing.equalToSuperview()
 		}
 	}
-	
-	override func prepareForReuse() {
-		super.prepareForReuse()
+}
+
+// MARK: - Internal Methods
+extension TermsOfUseCell {
+	func configuration(_ termsOfUse: TermsOfUse) {
+		let prefix = termsOfUse.isRequired ? "[필수]" : "[선택]"
+		termsOfUseButton.setTitle("\(prefix) \(termsOfUse.title)")
+		subTitleLabel.attributedText = termsOfUse.subTitle?.textmg(color: .gray80)
 	}
 	
-	func configuration(_ termsOfUse: TermsOfUse) {
-		setupUI(termsOfUse: termsOfUse)
+	func toggleButtonChecked() {
+		self.termsOfUseButton.isChecked.toggle()
+	}
+	
+	func setButtonChecked(_ isChecked: Bool) {
+		self.termsOfUseButton.isChecked = isChecked
 	}
 }
