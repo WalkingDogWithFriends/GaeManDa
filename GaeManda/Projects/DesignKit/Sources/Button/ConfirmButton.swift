0471612d9ca1ef3cb382f69ee010058c0d8b6b27
김영균 <48887389+klmyoungyun@users.noreply.github.com>
2023-08-29 20:22:42 +0900

    AddressSettingView 리팩토링 (#54)
    * [DESIGN]: DesignSystem ConfirmButton 추가
    
    * [REFACTOR]: AddressSettingView 리팩토링
    
    * [FEAT]: 확인 버튼 배경색 변경 로직 수정
    
    - isEnabled 속성 삭제 후 상태에 따라 배경색만 바뀌도록 변경

---
+++ GaeManda/Projects/DesignKit/Sources/Button/ConfirmButton.swift
@@ -0,0 +1,61 @@
+//
+//  ConfirmButton.swift
+//  DesignKit
+//
+//  Created by 김영균 on 2023/08/28.
+//  Copyright © 2023 com.gaemanda. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+public final class ConfirmButton: UIButton {
+	// MARK: - Properties
+	public var isPositive: Bool {
+		didSet {
+			isPositive ? setPositiveState() : setNegativeState()
+		}
+	}
+	
+	// MARK: - Initializers
+	public init(title: String) {
+		self.isPositive = false
+		super.init(frame: .zero)
+		
+		self.layer.cornerRadius = 4
+		self.backgroundColor = .gray60
+		self.setTitleColor(.white, for: .normal)
+		self.setTitle(title, for: .normal)
+		self.titleLabel?.font = .b16
+		self.isEnabled = false
+	}
+	
+	@available(*, unavailable)
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+	
+	// MARK: - Methods
+	private func setPositiveState() {
+		backgroundColor = .green100
+	}
+	
+	private func setNegativeState() {
+		backgroundColor = .gray60
+	}
+}
+
+public extension Reactive where Base: ConfirmButton {
+	var isPositive: ControlProperty<Bool> {
+		return controlProperty(
+			editingEvents: [.allEditingEvents, .valueChanged],
+			getter: { button in
+				button.isPositive
+			},
+			setter: { button, isPositive in
+				button.isPositive = isPositive
+			}
+		)
+	}
+}
