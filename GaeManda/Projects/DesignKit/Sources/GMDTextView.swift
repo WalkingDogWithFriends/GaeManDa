0619cf873939b3c612c59b5f075ae1dbdde65e4b
jungseokyoung-cloud <thrdud0423@gmail.com>
2023-09-06 00:17:59 +0900

    [REFACT]: GMDTextView 수정

--- GaeManda/Projects/DesignKit/Sources/GMDTextView.swift
+++ GaeManda/Projects/DesignKit/Sources/GMDTextView.swift
@@ -1,19 +1,41 @@
 import UIKit
+import RxCocoa
+import RxSwift
 import SnapKit
+import GMDExtensions
+
+public enum GMDTextViewMode {
+	case warning
+	case normal
+}
 
 public final class GMDTextView: UIView {
-	public lazy var isWarning = false {
+	// MARK: - Properties
+	public var mode: GMDTextViewMode = .normal {
 		didSet {
-			isWarning ? changeWarningMode() : changeNormalMode()
+			switch mode {
+			case .normal:
+				changeNormalMode()
+			case .warning:
+				changeWarningMode()
+			}
 		}
 	}
 	
-	public lazy var warningText: String = "" {
-		didSet {
-			warningLabel.text = warningText
+	public var text: String {
+		get {
+			textView.text ?? ""
+		}
+		set {
+			textView.text = newValue
+//			textView.refreshControl?.sendActions(for: .valueChanged)
 		}
 	}
 	
+	/// 최대 TextCount를 설정합니다.
+	public private(set) var maxTextCount: Int
+		
+	// MARK: - UI Components
 	private let stackView: UIStackView = {
 		let stackView = UIStackView()
 		stackView.spacing = 8
@@ -62,7 +84,7 @@ public final class GMDTextView: UIView {
 		return textView
 	}()
 	
-	private lazy var warningLabel: UILabel = {
+	public let warningLabel: UILabel = {
 		let label = UILabel()
 		label.textColor = .red100
 		label.numberOfLines = 1
@@ -72,33 +94,38 @@ public final class GMDTextView: UIView {
 		return label
 	}()
 	
-	public init(title: String) {
+	// MARK: - Initializer
+	public init(
+		title: String,
+		maxTextCount: Int = 100
+	) {
+		self.maxTextCount = maxTextCount
 		super.init(frame: .zero)
 		
 		titleLabel.text = title
+		warningLabel.text = "\(maxTextCount)자 이내로 입력 가능합니다."
 		setupUI()
 	}
 	
-	public convenience init(
-		title: String,
-		warningText: String
-	) {
-		self.init(title: title)
-		self.warningText = warningText
-	}
-	
+	@available(*, unavailable)
 	required init?(coder: NSCoder) {
 		fatalError("init(coder:) has not been implemented")
 	}
+}
+
+// MARK: - UI Settings
+private extension GMDTextView {
+	func setupUI() {
+		setViewHierarchy()
+		setConstraints()
+	}
 	
-	private func setupUI() {
-		addSubview(labelStackView)
-		addSubview(textView)
-		addSubview(warningLabel)
-
-		labelStackView.addArrangedSubview(titleLabel)
-		labelStackView.addArrangedSubview(maximumTextCountLabel)
-
+	func setViewHierarchy() {
+		addSubviews(labelStackView, textView, warningLabel)
+		labelStackView.addArrangedSubviews(titleLabel, maximumTextCountLabel)
+	}
+	
+	func setConstraints() {
 		labelStackView.snp.makeConstraints { make in
 			make.top.leading.trailing.equalToSuperview()
 		}
@@ -125,5 +152,12 @@ private extension GMDTextView {
 	func changeWarningMode() {
 		textView.layer.borderColor = UIColor.red100.cgColor
 		warningLabel.layer.opacity = 1.0
+	}
+}
+
+// MARK: - Reactive Extension
+public extension Reactive where Base: GMDTextView {
+	var text: ControlProperty<String?> {
+		base.textView.rx.text
 	}
 }
