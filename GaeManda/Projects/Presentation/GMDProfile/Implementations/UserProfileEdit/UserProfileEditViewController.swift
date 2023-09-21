b08d15b26d8618980b860e7fde7bf22d47db52f2
Jung SeokYoung <thrdud0423@gmail.com>
2023-09-21 20:19:49 +0900

    [BUG]: 키보드 안내려가는 버그 수정

--- GaeManda/Projects/Presentation/GMDProfile/Implementations/UserProfileEdit/UserProfileEditViewController.swift
+++ GaeManda/Projects/Presentation/GMDProfile/Implementations/UserProfileEdit/UserProfileEditViewController.swift
@@ -109,6 +109,11 @@ final class UserProfileEditViewController:
 		
 		hideTabBar()
 	}
+	// MARK: - touchesBegan
+	override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+		super.touchesBegan(touches, with: event)
+		self.view.endEditing(true)
+	}
 }
 
 // MARK: - UI Setting
