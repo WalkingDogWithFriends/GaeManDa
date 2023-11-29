f2ce93cf8b328af436c449324701c714ff885e9e
Jung SeokYoung <thrdud0423@gmail.com>
2023-11-29 22:23:09 +0900

    [FIX]: 코드 리뷰 반영

--- GaeManda/Projects/Data/GMDNetwork/MultipartFormData/MultipartFormDataBody.swift
+++ GaeManda/Projects/Data/GMDNetwork/MultipartFormData/MultipartFormDataBody.swift
@@ -74,7 +74,7 @@ struct MultipartFormDataBody {
 }
 
 // MARK: - NSMutableData Extension
-extension NSMutableData {
+fileprivate extension NSMutableData {
 	func appendString(_ string: String) {
 		if let data = string.data(using: .utf8) {
 			self.append(data)
