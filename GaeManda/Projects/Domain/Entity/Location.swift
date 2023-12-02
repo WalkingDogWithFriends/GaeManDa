58e20ba6dcd8589c726f88232cc9522897754021
Jung SeokYoung <thrdud0423@gmail.com>
2023-12-02 11:22:10 +0900

    [REFACT]: UserEntity 수정
    [FEAT]: Location Entity 추가

---
+++ GaeManda/Projects/Domain/Entity/Location.swift
@@ -0,0 +1,17 @@
+//
+//  Location.swift
+//  Entity
+//
+//  Created by jung on 12/1/23.
+//  Copyright © 2023 com.gaemanda. All rights reserved.
+//
+
+public struct Location {
+	public let latitude: Double
+	public let longitude: Double
+	
+	public init(latitude: Double, longitude: Double) {
+		self.latitude = latitude
+		self.longitude = longitude
+	}
+}
