5c2d4489055ff6bc5b32ef6fb5c2cbbf73c4e061
Jung SeokYoung <thrdud0423@gmail.com>
2023-12-16 01:22:30 +0900

    [REFACT]: Listener 함수 파라미터 수정
    [REFACT]: Listener 수정한거 적용

--- GaeManda/Projects/Presentation/GMDProfile/Implementations/NewDogProfile/NewDogProfileViewController.swift
+++ GaeManda/Projects/Presentation/GMDProfile/Implementations/NewDogProfile/NewDogProfileViewController.swift
@@ -59,8 +59,8 @@ final class NewDogProfileViewController:
 	override func viewWillDisappear(_ animated: Bool) {
 		super.viewWillDisappear(animated)
 		
-		removeKeyboardNotification([keyboardShowNotification, keyboardHideNotification])
-		removeTextFieldNotification([textDidChangeNotification])
+		removeKeyboardNotification(keyboardShowNotification, keyboardHideNotification)
+		removeTextFieldNotification(textDidChangeNotification)
 	}
 	
 	override func viewDidDisappear(_ animated: Bool) {
