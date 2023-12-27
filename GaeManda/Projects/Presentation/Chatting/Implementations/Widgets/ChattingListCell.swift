c48f8816e91ca7afa9b627f10e2262fb1c5e03d1
김영균 <48887389+klmyoungyun@users.noreply.github.com>
2023-12-27 13:49:44 +0900

    채팅 목록 UI 수정 및 삭제 구현 (#170)
    * [FEAT]: 채팅탭 UI 구현
    
    * [FEAT]: 오른쪽 네비게이션 버튼 추가
    
    * [FEAT]: 채팅 목록 삭제 구현
    
    * [FIX]
    
    ---------
    
    Co-authored-by: Jung SeokYoung <thrdud0423@gmail.com>
    Co-authored-by: jungseokyoung-cloud <81402827+jungseokyoung-cloud@users.noreply.github.com>

--- GaeManda/Projects/Presentation/Chatting/Implementations/Widgets/ChattingListCell.swift
+++ GaeManda/Projects/Presentation/Chatting/Implementations/Widgets/ChattingListCell.swift
@@ -7,26 +7,18 @@
 //
 
 import UIKit
-import RxCocoa
-import RxSwift
 import SnapKit
 import DesignKit
 import GMDExtensions
 
 final class ChattingListCell: UITableViewCell {
-	let disposeBag = DisposeBag()
-	
 	// MARK: UI Components
-	private let profileImageView: RoundImageView = {
-		let imageView = RoundImageView()
-		imageView.backgroundColor = .gray40
-		
-		return imageView
-	}()
+	private let profileImageView = RoundImageView()
 	
 	private let nickNameLabel: UILabel = {
 		let label = UILabel()
 		label.font = .b16
+		label.textColor = .black
 		
 		return label
 	}()
@@ -42,68 +34,119 @@ final class ChattingListCell: UITableViewCell {
 	private let messageLabel: UILabel = {
 		let label = UILabel()
 		label.font = .r12
+		label.textColor = .black
 		label.numberOfLines = 1
 		
 		return label
 	}()
-
+	
+	private let alarmOffImageView = UIImageView(image: .iconBellOff)
+	
+	private let newMessageCountLabel: UILabel = {
+		let label = UILabel()
+		label.font = .r8
+		label.textColor = .white
+		label.backgroundColor = .green100
+		label.textAlignment = .center
+		label.layer.cornerRadius = 8
+		label.clipsToBounds = true
+		label.isHidden = true
+		
+		return label
+	}()
+	
+	// MARK: - Initializers
 	override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
 		super.init(style: style, reuseIdentifier: reuseIdentifier)
 		self.selectionStyle = .none
 		setupUI()
 	}
-
+	
+	@available(*, unavailable)
 	required init?(coder: NSCoder) {
-		fatalError()
+		fatalError("init(coder:) has not been implemented")
 	}
 	
 	override func prepareForReuse() {
 		super.prepareForReuse()
+		profileImageView.cancel()
 	}
 	
-	func configure() {
-		nickNameLabel.text = "윈터"
-		dateLabel.text = "1달 전"
-		messageLabel.text = "안녕하세요, 카리나님! 오늘 초코랑 같이 산책하실래요?"
+	func configure(with viewModel: ChattingListDataSource.ViewModel) {
+		profileImageView.loadImage(urlString: viewModel.profileImage)
+		nickNameLabel.text = viewModel.nickName
+		dateLabel.text = viewModel.recentMessageDate
+		messageLabel.text = viewModel.message
+		
+		if let newMessageCount = viewModel.newMessageCount {
+			newMessageCountLabel.isHidden = false
+			newMessageCountLabel.text = "\(newMessageCount)"
+		}
+		alarmOffImageView.isHidden = viewModel.isAlarmOn
 	}
 }
 
 // MARK: UI Setting
 private extension ChattingListCell {
+	enum Constant {
+		static let contentViewInsets = UIEdgeInsets(top: 12, left: 32, bottom: 12, right: 32)
+		static let profileImageSize = CGSize(width: 60, height: 60)
+		static let profileImageOutsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 20)
+		static let nickNameLabelBottomPadding: CGFloat = 20
+		static let alarmOffImageViewLeadingPadding: CGFloat = 10
+		static let alarmOffImageViewSize = CGSize(width: 12, height: 14)
+		static let newMessageLabelSize = CGSize(width: 16, height: 16)
+	}
+	
 	func setupUI() {
-		setupSubviews()
+		setViewHierarchy()
 		setConstraints()
 	}
 	
-	func setupSubviews() {
-		contentView.addSubview(profileImageView)
-		contentView.addSubview(nickNameLabel)
-		contentView.addSubview(dateLabel)
-		contentView.addSubview(messageLabel)
+	func setViewHierarchy() {
+		contentView.addSubviews(
+			profileImageView, nickNameLabel, alarmOffImageView, dateLabel, messageLabel, newMessageCountLabel
+		)
 	}
 	
 	func setConstraints() {
 		profileImageView.snp.makeConstraints { make in
-			make.width.height.equalTo(60)
-			make.leading.equalToSuperview().offset(32)
-			make.bottom.equalTo(messageLabel.snp.bottom)
+			make.size.equalTo(Constant.profileImageSize)
+			make.leading.equalToSuperview().offset(Constant.contentViewInsets.left)
+			make.top.equalToSuperview().offset(Constant.contentViewInsets.top)
+			make.bottom.equalToSuperview().offset(-Constant.contentViewInsets.bottom)
 		}
 		
 		nickNameLabel.snp.makeConstraints { make in
-			make.leading.equalTo(profileImageView.snp.trailing).offset(20)
-			make.top.equalToSuperview().offset(28)
+			make.leading.equalTo(profileImageView.snp.trailing).offset(Constant.profileImageOutsets.right)
+			make.top.equalTo(profileImageView)
+		}
+		
+		alarmOffImageView.snp.makeConstraints { make in
+			make.centerY.equalTo(nickNameLabel)
+			make.leading.equalTo(nickNameLabel.snp.trailing).offset(Constant.alarmOffImageViewLeadingPadding)
+			make.size.equalTo(Constant.alarmOffImageViewSize)
 		}
 		
 		dateLabel.snp.makeConstraints { make in
-			make.leading.equalTo(nickNameLabel.snp.trailing).offset(12)
-			make.centerY.equalTo(nickNameLabel.snp.centerY)
+			make.trailing.equalToSuperview().offset(-Constant.contentViewInsets.right)
+			make.centerY.equalTo(nickNameLabel)
 		}
 		
 		messageLabel.snp.makeConstraints { make in
-			make.leading.equalTo(profileImageView.snp.trailing).offset(20)
-			make.top.equalTo(nickNameLabel.snp.bottom).offset(20)
-			make.bottom.equalToSuperview().offset(-16)
-			make.trailing.equalToSuperview().offset(-32)
+			make.leading.equalTo(profileImageView.snp.trailing).offset(Constant.profileImageOutsets.right)
+			make.bottom.equalTo(profileImageView)
 		}
+		
+		newMessageCountLabel.snp.makeConstraints { make in
+			make.width.greaterThanOrEqualTo(Constant.newMessageLabelSize.width)
+			make.height.equalTo(Constant.newMessageLabelSize.height)
+			make.centerY.equalTo(messageLabel)
+			make.trailing.equalToSuperview().offset(-Constant.contentViewInsets.right)
+			make.leading.equalTo(messageLabel.snp.trailing).offset(8)
+		}
+		
+		newMessageCountLabel.setContentHuggingPriority(.init(755), for: .horizontal)
+		newMessageCountLabel.setContentCompressionResistancePriority(.init(755), for: .horizontal)
 	}
 }
